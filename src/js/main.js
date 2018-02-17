28fcb6508f86b97c3fc7019c521e2cdfe7fec9ef
vishalmshah <vishah2001@gmail.com>
2018-02-16 20:11:43 -0500

    Updated Logo

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
   $nav = $('nav');
   $fill = nthOfType('.fill', 4);
   $mask = $('.mask');
+  $filler = nthOfType('.filler', 4);
+  $masking = $('.masking');
 
   if (window.innerWidth > 800) {
     // startup($loader, $loaderTransition, $loadingText, $gridLeftItem);
@@ -11,6 +13,10 @@ $(document).ready(function(){
   }
 
   startup($fill, $mask)
+
+
+  // TODO: Start this at scroll down
+  seeGetStarted($filler, $masking)
 
   $menuBtn.on('click', function() {
     openNav($nav)
@@ -30,9 +36,15 @@ function nthOfType(type, num) {
 }
 
 function startup($fill, $mask) {
-
   for(var i = 0; i < $fill.length; i++){
     $fill[i].toggleClass('started')
   }
   $mask.toggleClass('started')
+}
+
+function seeGetStarted($filler, $masking) {
+  for(var i = 0; i < $filler.length; i++){
+    $filler[i].toggleClass('started')
+  }
+  $masking.toggleClass('started')
 }
