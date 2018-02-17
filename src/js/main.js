fe7645dbd7e6bf9f2e6541b583a15c181b8f3a8d
vishalmshah <vishah2001@gmail.com>
2018-02-16 19:00:01 -0500

    Finished Landing Animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
   $menuBtn = $('.menuBtn');
   $nav = $('nav');
+  $fill = nthOfType('.fill', 4);
+  $mask = $('.mask');
 
   if (window.innerWidth > 800) {
     // startup($loader, $loaderTransition, $loadingText, $gridLeftItem);
   } else {
     // mobileStartup($gridLeftItem)
   }
+
+  startup($fill, $mask)
 
   $menuBtn.on('click', function() {
     openNav($nav)
@@ -15,4 +19,20 @@ $(document).ready(function(){
 
 function openNav($nav) {
   $nav.toggleClass('selected')
+}
+
+function nthOfType(type, num) {
+  var typeArray = []
+  for(var i = 1; i <= num; i++) {
+    typeArray.push($(type + ':nth-of-type(' + i + ')'));
+  }
+  return typeArray
+}
+
+function startup($fill, $mask) {
+
+  for(var i = 0; i < $fill.length; i++){
+    $fill[i].toggleClass('started')
+  }
+  $mask.toggleClass('started')
 }
