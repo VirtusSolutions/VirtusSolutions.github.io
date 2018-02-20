b1e1f9949d70e263715feb86e9d7b52366d3ee16
vishalmshah <vishah2001@gmail.com>
2018-02-20 17:12:53 -0500

    Almost done
    Responsive hover, nice animations, etc. Alsmost done other than form and content.

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
+  $window = $(window);
+  $body = $('body')
   $menuBtn = $('.menuBtn');
   $nav = $('nav');
   $fill = nthOfType('.fill', 4);
   $mask = $('.mask');
   $filler = nthOfType('.filler', 4);
   $masking = $('.masking');
+  $link = $('.link a');
+  $start = $('#start');
+
 
   if (window.innerWidth > 800) {
     // startup($loader, $loaderTransition, $loadingText, $gridLeftItem);
@@ -14,13 +19,34 @@ $(document).ready(function(){
 
   startup($fill, $mask)
 
-
   // TODO: Start this at scroll down
-  seeGetStarted($filler, $masking)
+  // seeGetStarted($filler, $masking)
 
   $menuBtn.on('click', function() {
     openNav($nav)
   });
+
+  $link.on('click', function() {
+    openNav($nav)
+  });
+
+  // For seeGetStarted
+  var element_position = $start.offset().top;
+  var screen_height = $window.height();
+  var activation_offset = 0.65;//determines how far up the the page the element needs to be before triggering the function
+  var activation_point = element_position - (screen_height * activation_offset);
+  var max_scroll_height = $body.height() - screen_height - 5;//-5 for a little bit of buffer
+  $(window).on('scroll', function() {
+    var y_scroll_pos = window.pageYOffset;
+
+    var element_in_view = y_scroll_pos > activation_point;
+    var has_reached_bottom_of_page = max_scroll_height <= y_scroll_pos && !element_in_view;
+
+    if(element_in_view || has_reached_bottom_of_page) {
+        seeGetStarted($filler, $masking)
+    }
+  })
+
 });
 
 function openNav($nav) {
@@ -44,7 +70,7 @@ function startup($fill, $mask) {
 
 function seeGetStarted($filler, $masking) {
   for(var i = 0; i < $filler.length; i++){
-    $filler[i].toggleClass('started')
+    $filler[i].addClass('started')
   }
-  $masking.toggleClass('started')
+  $masking.addClass('started')
 }
